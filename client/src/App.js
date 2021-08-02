26cd71867a83d8b34a5dbe87ea914aa2cca4affb
Dimo Dimchev <ddimchev7@gmail.com>
2021-08-02 19:34:37 +0300

    Moved rooms to a global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import Rooms from "./components/Rooms";
 import Chat from "./components/Chat";
 
 function App() {
+  const [rooms, setRooms] = React.useState([]);
+
   return (
     <div className="App">
-      <Rooms />
+      <Rooms rooms={rooms} setRooms={setRooms} />
       <Chat />
     </div>
   );
--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Room from "./Room";
 
-const Rooms = () => {
-  const [tasks, setTasks] = React.useState([]);
-
+const Rooms = ({ rooms, setRooms }) => {
   // render all rooms
   React.useEffect(() => {
     const getRooms = async () => {
       const res = await fetch("http://localhost:5000/");
       const data = await res.json();
-      setTasks(data);
+      setRooms(data);
     };
     getRooms();
   }, []);
@@ -18,8 +16,8 @@ const Rooms = () => {
     <div className="side-menu">
       <h2>All rooms</h2>
       <div className="rooms">
-        {tasks.map((task) => (
-          <Room key={task._id} name={task.name} />
+        {rooms.map((room) => (
+          <Room key={room._id} name={room.name} />
         ))}
       </div>
     </div>
