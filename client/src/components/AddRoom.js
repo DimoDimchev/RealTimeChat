41888bbf54cf69c9495eab75180d85e51e66d0fe
Dimo Dimchev <ddimchev7@gmail.com>
2021-08-02 20:37:55 +0300

    Added form and implemented method to add rooms

---
+++ client/src/components/AddRoom.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const AddRoom = () => {
+  const [name, setName] = React.useState();
+
+  const addRoom = async () => {
+    await fetch("http://localhost:5000/rooms", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ roomName: name }),
+    });
+  };
+  return (
+    <form className="room-form">
+      <input
+        type="text"
+        placeholder="Add room name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button type="submit" onClick={addRoom}>
+        Add room
+      </button>
+    </form>
+  );
+};
+
+export default AddRoom;
--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Room from "./Room";
+import AddRoom from "./AddRoom";
 
 const Rooms = ({ rooms, setRooms }) => {
   // render all rooms
@@ -15,6 +16,7 @@ const Rooms = ({ rooms, setRooms }) => {
   return (
     <div className="side-menu">
       <h2>All rooms</h2>
+      <AddRoom />
       <div className="rooms">
         {rooms.map((room) => (
           <Room key={room._id} name={room.name} />
