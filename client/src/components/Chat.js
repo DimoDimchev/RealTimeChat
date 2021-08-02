5f29549a603b82f267b142e7494537015d17c9ce
Dimo Dimchev <ddimchev7@gmail.com>
2021-08-02 14:53:28 +0300

    Removed garbage | Started styling

---
+++ client/src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="chat-container">
+      <h2>Chat</h2>
+      <div className="chat-box"></div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ client/src/components/SideMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SideMenu = () => {
+  return (
+    <div className="side-menu">
+      <h2>All rooms</h2>
+      <div className="rooms"></div>
+    </div>
+  );
+};
+
+export default SideMenu;
